ff13314888bf191192ebbc6b181ee091d2d0b98c
Camila Ferreira <camila.ferreira@bluesoft.com.br>
2019-04-03 18:19:59 -0300

    #184025-Criando a classe Menu e Opção

--- MP3PadawanProject/src/br/com/padawan/Main.java
+++ MP3PadawanProject/src/br/com/padawan/Main.java
@@ -1,14 +1,35 @@
 package br.com.padawan;
 
+import br.com.padawan.Menu.Menu;
+import br.com.padawan.Menu.Opcao;
 import br.com.padawan.aparelhomp3.MP3;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MP3 aparelhoMP3 = new MP3();
+        Opcao cadastrarMusica = new Opcao(1,"Cadastrar músicas");
+        Opcao consultarMusica = new Opcao(2,"Consultar músicas");
+        Opcao excluirMusica = new Opcao(3,"Excluir músicas");
+        Opcao consultarBibliotecaPlayList = new Opcao(4,"Biblioteca / PlayList");
+        Opcao cadastrarPlayList = new Opcao(5,"Cadastrar PlayList");
+        Opcao adicionarMusicasPlayList = new Opcao(6,"Adicionar Músicas na Playlist");
+        Opcao desligarMp3 = new Opcao(7,"Desligar MP3");
+
+        Menu menuOpcoesMp3 = new Menu();
+        menuOpcoesMp3.adicionarOpcao(cadastrarMusica);
+        menuOpcoesMp3.adicionarOpcao(consultarMusica);
+        menuOpcoesMp3.adicionarOpcao(excluirMusica);
+        menuOpcoesMp3.adicionarOpcao(consultarBibliotecaPlayList);
+        menuOpcoesMp3.adicionarOpcao(cadastrarPlayList);
+        menuOpcoesMp3.adicionarOpcao(adicionarMusicasPlayList);
+        menuOpcoesMp3.adicionarOpcao(desligarMp3);
+
+        MP3 aparelhoMP3 = new MP3(menuOpcoesMp3);
 
         aparelhoMP3.liga();
 
     }
+
+
 }
